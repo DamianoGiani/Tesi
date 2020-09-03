0a67100ce2b262c6f945e27aae37d32975699002
DamianoGiani <damiano.giani@gmail.com>
2020-09-03 16:57:46 +0200

    Add files via upload

--- readpy.py
+++ readpy.py
@@ -9,16 +9,34 @@ with open("temp.bin", "rb") as f:
         byte = f.read(1)
 #print(array)
 list=[]
-for x in range(0, len(array)):
+for x in range(0, 14):
     k = np.frombuffer(array[x], dtype=np.uint8)
     list.append(np.unpackbits(k).reshape(8))
 list = np.array(list)
 l=len(list)
 my_list=(np.reshape(list,l*8))
+pixel=[]
 
+for x in range(0,int(len(my_list)/14)):
+    pixel.append(my_list[x* 14: x*14 + 14])
+
+pixel=np.array(pixel)
+print(pixel[0])
+rgbarray=[]
+for x in range(0,len(pixel)):
+    i = (pixel[x].dot(2 ** np.arange(14)[::-1]))
+    rgbarray.append(int(round((i/16383)*255)))
+rgbarray=np.array(rgbarray)
+print(rgbarray)
+"""
+i=(pixel[0].dot(2**np.arange(14)[::-1]))
+print(i)
+i=int(round((i/16383)*255))
+print(i)
+"""
 #list=list.tolist()
 
-
+"""
 '''
 def access_bit(data, num):
     base = int(num // 8)
@@ -30,7 +48,9 @@ print(x)'''
 #x=[access_bit(array,i) for i in range(len(array)*8)]
 
 #firstfivebits = byte >> 2
-"""while byte:
+"""
+"""
+while byte:
     print(byte)
     byte = file.read(1)
 file.close()"""
